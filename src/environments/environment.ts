931a8ab1b44be7a1fce13114273c7800525600e1
KathJC <42304004+KathJC@users.noreply.github.com>
2021-10-20 17:21:20 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAcgSu-Dme82nSG9Gye9us2pXM2uuBORgo",
+    authDomain: "app-map-55cdd.firebaseapp.com",
+    databaseURL: "https://app-map-55cdd-default-rtdb.firebaseio.com/",
+    projectId: "app-map-55cdd",
+    storageBucket: "app-map-55cdd.appspot.com",
+    messagingSenderId: "706419361661",
+    appId: "1:706419361661:web:af1c9f32d288d86d92ae14",
+    measurementId: "G-08NYYF665P"
+  }
+};
+
+
+  
+ 
